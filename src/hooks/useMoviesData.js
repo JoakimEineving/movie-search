f3536c64e8e64fff487967d75bac7a1872a81e33
joakimeineving <Joakim.eineving@gmail.com>
2023-02-26 20:54:11 +0100

    hooks folder

---
+++ src/hooks/useMoviesData.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { getSearchMovies, getPopularMovies } from "../Services/MovieService";
+
+const useMoviesData = (searchValue, year) => {
+  const [searchMovies, setSearchMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [starredMovies, setStarredMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const popularMoviesData = await getPopularMovies();
+      setPopularMovies(popularMoviesData);
+
+      const starredMoviesData =
+        JSON.parse(localStorage.getItem("starredMovies")) || [];
+      setStarredMovies(starredMoviesData);
+
+      if (!searchValue) {
+        const lastSearchResults =
+          JSON.parse(localStorage.getItem("lastSearchResults")) || [];
+        setSearchMovies(lastSearchResults);
+      } else {
+        const searchMoviesData = await getSearchMovies(searchValue, year);
+        setSearchMovies(searchMoviesData);
+        localStorage.setItem(
+          "lastSearchResults",
+          JSON.stringify(searchMoviesData || [])
+        );
+      }
+    };
+    fetchData();
+  }, [searchValue, year]);
+
+  return { searchMovies, popularMovies, starredMovies };
+};
+
+export default useMoviesData;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,48 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import MovieList from "../components/MovieList";
 import SearchBar from "../components/SearchBar";
-import { getSearchMovies, getPopularMovies } from "../api/api";
+
 import "../styles/LandingPage.scss";
 import Navbar from "../components/Navbar";
+import useMoviesData from "../hooks/useMoviesData";
 
 const LandingPage = () => {
   const [searchValue, setSearchValue] = useState("");
   const [year, setYear] = useState("");
-  const [searchMovies, setSearchMovies] = useState([]);
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [starredMovies, setStarredMovies] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const popularMoviesData = await getPopularMovies();
-      setPopularMovies(popularMoviesData);
-
-      const starredMoviesData = JSON.parse(localStorage.getItem("starredMovies")) || [];
-      setStarredMovies(starredMoviesData);
-      console.log(starredMoviesData);
-    
-      if (!searchValue) {
-        const lastSearchResults =
-          JSON.parse(localStorage.getItem("lastSearchResults")) || [];
-        setSearchMovies(lastSearchResults);
-      } else {
-        const searchMoviesData = await getSearchMovies(searchValue, year);
-        setSearchMovies(searchMoviesData);
-        localStorage.setItem(
-          "lastSearchResults",
-          JSON.stringify(searchMoviesData)
-        );
-      }
-    };
-    fetchData();
-  }, [searchValue,year]);
-
+  const { searchMovies, popularMovies, starredMovies } = useMoviesData(
+    searchValue,
+    year
+  );
   return (
     <div className="wrapper-landing">
       <Navbar />
-      
-      
-      <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} year={year}setYear={setYear} />
+
+      <SearchBar
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        year={year}
+        setYear={setYear}
+      />
       {!searchValue && localStorage.getItem("lastSearchResults") ? (
         <h2>Previous Search</h2>
       ) : (
